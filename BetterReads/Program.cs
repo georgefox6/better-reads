77209a0de4c15307bad5252f89a0aef6e0d36570
George Fox <george.fox@razor.co.uk>
2023-01-09 18:47:48 +0000

    Added xml documentation to swagger

--- BetterReads/Program.cs
+++ BetterReads/Program.cs
@@ -2,12 +2,13 @@ using BetterReads.Api.Exceptions.Infrastructure;
 using BetterReads.Data;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,7 +21,20 @@ builder.Services.AddDbContext<BetterReadsContext>(options =>
 
 builder.Services.AddMediatR(typeof(Program).Assembly).AddExceptionHandlers();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Better Reads",
+        Description = "Keep track of all the books you want to read, are currently reading and have read.",
+        TermsOfService = null,
+        Contact = new OpenApiContact
+            { Name = "George Fox", Email = "george.fox@razor.co.uk", Url = null }
+    });
+    var filePath = Path.Combine(AppContext.BaseDirectory, "BetterReads.Api.xml");
+    c.IncludeXmlComments(filePath);
+});
 
 var app = builder.Build();
 
@@ -34,11 +48,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpContextExceptionHandling();
 
